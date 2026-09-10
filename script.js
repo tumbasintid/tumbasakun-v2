d9e528b94781e6025846485b29e0e7c63748d647
tumbasintid <aksaralokaa1@gmail.com>
2026-09-11 02:14:18 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Menunggu seluruh elemen web dimuat
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website Tumbas Akun V2 siap digunakan!");
+});
+
+function sapaPembeli() {
+    alert("Selamat datang di Tumbas Akun! Silakan pilih produk pilihanmu.");
+}
